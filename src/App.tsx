3482ea509aa26c194e4aa836173a25fd4f36320d
deepakMS12 <deepak.dhiman@mscorpres.in>
2025-07-05 12:53:54 +0530

    fix bug in store

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { route } from "../routes";
 import { AuthProvider } from "./contextapi/AuthContext";
-import { Provider } from "react-redux";
-import { store } from "./features/store";
+
 import { ToastContext } from "./contextapi/ToastContext";
 
 function App() {
   return (
-    <Provider store={store}>
-      <ToastContext>
-        <AuthProvider>
-          <RouterProvider router={route} />
-        </AuthProvider>
-      </ToastContext>
-    </Provider>
+    <ToastContext>
+      <AuthProvider>
+        <RouterProvider router={route} />
+      </AuthProvider>
+    </ToastContext>
   );
 }
 
